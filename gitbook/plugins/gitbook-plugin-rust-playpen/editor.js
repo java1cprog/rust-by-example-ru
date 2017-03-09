6c444249a50ce5858caef39722e05f304d95e356
Igor.Shaposhnik <shaposhnikigor95@bk.ru>
2017-03-09 09:32:28 +0000

    rebuild pages at e17f9c3

--- gitbook/plugins/gitbook-plugin-rust-playpen/editor.js
+++ gitbook/plugins/gitbook-plugin-rust-playpen/editor.js
@@ -70,7 +70,7 @@ function initEditor() {
     runProgram(program, handleResult);
   };
 
-  ace.config.setModuleUrl('ace/mode/rust', '../gitbook/plugins/gitbook-plugin-rust-playpen/mode-rust.js');
+  ace.config.setModuleUrl('ace/mode/rust', '/rust-by-example-ru/gitbook/plugins/gitbook-plugin-rust-playpen/mode-rust.js');
 
   editor.setTheme("ace/theme/tomorrow");
   editor.getSession().setMode("ace/mode/rust");
